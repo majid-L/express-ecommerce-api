9b7323b9d50af588294a91e94737adf886530d5a
3782291211 <Laklouk98@hotmail.com>
2023-07-22 14:09:22 +0100

    Adding session config

--- auth/session.ts
+++ auth/session.ts
@@ -1,16 +1,45 @@
 import { Application } from "express";
 import session from 'express-session';
+import connectPg from 'connect-pg-simple';
+import pg from 'pg';
+import 'dotenv/config';
+
+declare module 'express-session' {
+  interface SessionData {
+    passport?: {
+      user: {
+        id: number,
+        name: string,
+        username: string,
+        passowrd: string,
+        email: string,
+        join_date: string
+      }
+    }
+  }
+}
 
 const generateSession = (app: Application) => {
-  const store = new session.MemoryStore();
-  
+  const pgSession = connectPg(session);
+  const pgPool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 2
+  });
+
   app.use(
     session({
-      secret: 'dev_secret',
-      cookie: { maxAge: 3e8, secure: true, sameSite: 'none' },
+      secret: 'secret',
+      cookie: { 
+        maxAge: 30 * 24 * 60 * 60 * 1000, 
+        secure: process.env.DATABASE_URL!.includes('localhost') ? false : true, 
+        sameSite: 'none' 
+      },
       saveUninitialized: false,
       resave: false,
-      store
+      store: new pgSession({
+        pool: pgPool,
+        tableName: 'session'
+      })
     })
   );
 }
