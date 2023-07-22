38657578b42d150db7ff9e5052468c1ebaaab6b3
3782291211 <Laklouk98@hotmail.com>
2023-07-22 14:10:07 +0100

    Adding types

--- types/types.ts
+++ types/types.ts
@@ -6,3 +6,25 @@ export type User = {
   email: string,
   join_date: Date | string
 }
+
+export type Product = {
+  name: string,
+  description: string,
+  price: number,
+  stock: number,
+  category: string,
+  supplier: string
+}
+
+export type Category = {
+  name: string,
+  description: string,
+  thumbnail?: string
+}
+
+export type Supplier = {
+  name: string,
+  location: string,
+  establishYear: number,
+  thumbnail?: string
+}
