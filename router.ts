bc40ec8254abd04a1992947e24e2df6343e47baf
3782291211 <Laklouk98@hotmail.com>
2023-07-22 14:08:51 +0100

    Adding api endpoints

--- router.ts
+++ router.ts
@@ -1,10 +1,37 @@
 import { Router } from 'express';
-import { validateInput } from './middleware/validateInput';
-import { checkIfCustomerExists } from './middleware/checkIfCustomerExists';
-import { getCustomers, signup } from './controllers/app.controllers';
+import passport from 'passport';
+import { validateAuthInput } from './middleware/validateAuthInput';
+import { validateUniqueCredentials } from './middleware/validateAuth';
+import { getCustomers, signup, login, logout } from './controllers/app.controllers';
 
 const router = Router();
-router.get('/users', getCustomers);
-router.post('/signup', validateInput, checkIfCustomerExists, signup);
+
+// products
+router.get('/products', /*...*/);
+
+// single product
+router.get('/products/:productId', /*...*/);
+
+// account details
+router.get('/customers/:customerId', getCustomers);
+router.put('/customers/:customerId', /*...*/);
+router.delete('/customers/:customerId', /*...*/);
+
+// cart items
+router.get('/customers/:customerId/cart', /*...*/);
+router.post('/customers/:customerId/cart', /*...*/);
+router.put('/customers/:customerId/cart/:itemId', /*...*/);
+router.delete('/customers/:customerId/cart/:itemId', /*...*/);
+
+// orders
+router.get('/customers/:customerId/orders', /*...*/);
+router.post('/customers/:customerId/orders', /*...*/);
+
+// single order
+router.get('/customers/:customerId/orders/:orderId', /*...*/);
+
+router.post('/login', passport.authenticate('local'), login);
+router.post('/logout', logout);
+router.post('/signup', validateAuthInput, validateUniqueCredentials, signup);
 
 export default router;
