8a0d5c6ad5b046a9c233415082463c5bb670bc36
3782291211 <Laklouk98@hotmail.com>
2023-07-23 07:49:02 +0100

    Modifying passport serializer logic

--- auth/passport.ts
+++ auth/passport.ts
@@ -3,6 +3,14 @@ import localStrategyConfig from './localStrategy';
 import { Application } from 'express';
 import type { User } from '../types/types';
 
+const formatUserData = (user: User) => {
+  return {
+    id: user.id,
+    name: user.name,
+    username: user.username
+  };
+}
+
 const passportConfig = (app: Application) => {
   app.use(passport.initialize());
   app.use(passport.session());
@@ -11,12 +19,12 @@ const passportConfig = (app: Application) => {
 
   // stores user to session
   passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, formatUserData(user as User));
   });
   
   // retrieves user from session
   passport.deserializeUser((user, done) => {
-    done(null, user as User);
+    done(null, formatUserData(user as User));
   });
 };
 
