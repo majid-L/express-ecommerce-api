6f410c454b4d13ec1c26512c21cdcf643636a256
3782291211 <Laklouk98@hotmail.com>
2023-07-23 07:48:01 +0100

    Updating routes

--- router.ts
+++ router.ts
@@ -1,24 +1,26 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { validateAuthInput } from './middleware/validateAuthInput';
+import validateAuthInput from './middleware/validateAuthInput';
 import { validateUniqueCredentials } from './middleware/validateAuth';
-import { getCustomers, signup, login, logout } from './controllers/app.controllers';
+import customerIdParamHandler from './middleware/customerIdParamHandler';
+import { signup, login, logout, getProducts, getCustomerById, getCartItems } from './controllers/app.controllers';
 
 const router = Router();
+router.param('customerId', customerIdParamHandler);
 
 // products
-router.get('/products', /*...*/);
+router.get('/products', getProducts);
 
 // single product
 router.get('/products/:productId', /*...*/);
 
 // account details
-router.get('/customers/:customerId', getCustomers);
+router.get('/customers/:customerId', getCustomerById);
 router.put('/customers/:customerId', /*...*/);
 router.delete('/customers/:customerId', /*...*/);
 
 // cart items
-router.get('/customers/:customerId/cart', /*...*/);
+router.get('/customers/:customerId/cart', getCartItems);
 router.post('/customers/:customerId/cart', /*...*/);
 router.put('/customers/:customerId/cart/:itemId', /*...*/);
 router.delete('/customers/:customerId/cart/:itemId', /*...*/);
