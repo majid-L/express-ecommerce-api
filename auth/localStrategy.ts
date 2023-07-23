a8ed114862f0cb88c52f26d1e847b3cbd67b899d
3782291211 <Laklouk98@hotmail.com>
2023-07-23 07:48:11 +0100

    Updating local strategy

--- auth/localStrategy.ts
+++ auth/localStrategy.ts
@@ -1,18 +1,14 @@
-import passport from "passport"
 import { Strategy } from 'passport-local';
 import prisma from "../prisma/prisma";
 import bcrypt from 'bcrypt';
 
 const localStrategyConfig = () => {
   return new Strategy(
-    async function validate(username, password, done) {
+    async function verify(username, password, done) {
       try {
         const customerOrNull = await prisma.customer.findUnique({ 
           where: { username }
         });
-
-        const err: Error = new Error ('Invalid username or password.');
-        (err as Error & { status: number }).status = 400;
 
         if (!customerOrNull) {
           return done(null, false);
