f2ae5c56dcdc2a9b6d658a8452404bc10035ab45
3782291211 <Laklouk98@hotmail.com>
2023-07-24 17:22:22 +0100

    Adding authentication middleware to stack

--- index.ts
+++ index.ts
@@ -1,19 +1,24 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-import { handleError } from './middleware/errors';
-import router from './router';
-import localStrategy from './auth/localStrategy';
+import express from 'express';
+import { userIsAuthenticated } from './middleware/validateAuth';
+import cors from 'cors';
+import handleError from './middleware/errors';
+import apiRouter from './routers/apiRouter';
 import passportConfig from './auth/passport';
 import generateSession from './auth/session';
 
 const app: express.Application = express();
-
+app.use(cors());
 app.use(express.json());
 
 generateSession(app);
-localStrategy();
 passportConfig(app);
 
-app.use(router);
+app.use([
+    '/api/customers/:customerId',
+    '/api/logout'
+], userIsAuthenticated);
+
+app.use('/api', apiRouter);
 app.use(handleError);
 
 export default app;
