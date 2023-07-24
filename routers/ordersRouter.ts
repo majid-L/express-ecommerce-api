d2216452f5b0698eb9656e677c64375fabce06b6
3782291211 <Laklouk98@hotmail.com>
2023-07-24 11:28:30 +0100

    Adding orders subrouter

---
+++ routers/ordersRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createOrder, getOrderById, getOrders } from "../controllers/orders.controllers";
+
+const ordersRouter = Router();
+
+ordersRouter
+.route('/')
+.get(getOrders)
+.post(createOrder);
+
+ordersRouter.route('/:orderId').get(getOrderById);
+
+export default ordersRouter;
