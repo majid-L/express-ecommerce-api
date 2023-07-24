e9d03fa0b2beae1aee501638c810dcbee9ec9864
3782291211 <Laklouk98@hotmail.com>
2023-07-24 17:25:30 +0100

    Deleting/renaming/moving files

--- prisma/migrations/20230722021604_sessions/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Session" (
-    "sessionId" TEXT NOT NULL,
-    "session" JSONB NOT NULL,
-    "expiry" TIMESTAMP(6) NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Session_sessionId_key" ON "Session"("sessionId");
