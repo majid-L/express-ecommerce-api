e6e12f26db7623c08b64eef06887a82d0e1d6ac5
3782291211 <Laklouk98@hotmail.com>
2023-07-24 17:21:57 +0100

    Updating port

--- app.listen.ts
+++ app.listen.ts
@@ -1,5 +1,5 @@
 import app from "./index";
 
-app.listen(9090, () => {
-  console.log('Server is listening at port 9090');
+app.listen(3000, () => {
+  console.log('Server is listening at port 3000');
 });
