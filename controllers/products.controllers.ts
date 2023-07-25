6cb9590302ef8c7e7409b1ef9c69b7bc517f2f1b
3782291211 <Laklouk98@hotmail.com>
2023-07-25 20:46:20 +0100

    Adding query params and agg queries to /products

--- controllers/products.controllers.ts
+++ controllers/products.controllers.ts
@@ -3,18 +3,103 @@ import prisma from '../prisma/prisma';
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { limit = 25, page = 1 } = req.query;
-    const products = await prisma.product.findMany({
-      orderBy: {
-        id: 'asc',
+    const { 
+      limit = 25, 
+      page = 1,
+      minPrice = 0,
+      maxPrice = 1e5,
+      category,
+      supplier,
+      hideOutOfStock = false,
+      sortBy = 'id',
+      order = 'asc'
+    } = req.query;
+
+    const numberOfOrders = {
+      include: { _count: { select: { orderItems: true } } }
+    }
+
+    const queryOptions = {
+      where: {
+        AND: [ 
+          { NOT: {} },
+          {
+            price: { 
+              gte: Number(minPrice),
+              lte: Number(maxPrice)
+            }
+          },
+          { categoryName: {} },
+          { supplierName: {} }
+        ]
       },
-      skip: (Number(page) - 1) * Number(limit),
-      take: Number(limit)
-    })
-    res.send({products});
+      orderBy: {
+        [sortBy as string]: order,
+      }
+    };
+
+    if (hideOutOfStock) queryOptions.where.AND[0].NOT = { stock: 0 };
+    if (category) queryOptions.where.AND[2].categoryName = { 
+      contains: category,
+      mode: 'insensitive'
+     };
+    if (supplier) queryOptions.where.AND[3].supplierName = { 
+      contains: supplier,
+      mode: 'insensitive'
+    };
+
+    const resultSet = await prisma.$transaction([
+      prisma.product.count(queryOptions),
+      prisma.product.findMany({
+        ...queryOptions,
+        ...numberOfOrders,
+        skip: (Number(page) - 1) * Number(limit),
+        take: Number(limit),
+        })
+    ]);
+
+    res.send({
+      page: Number(page),
+      count: resultSet[1].length,
+      totalResults: resultSet[0],
+      products: resultSet[1].map((product: ProductWithOrderCount) => {
+        product.numOfTimesOrdered = product._count!.orderItems;
+        delete product._count;
+        return product;
+      })
+    });
   } catch (err) {
     next(err);
   }  
+}
+
+export const getBestSellers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { limit = 25, page = 1 } = req.query;
+    const bestSellers = await prisma.$queryRaw`
+      SELECT 
+        p."id", 
+        COUNT(oi."orderId")::INTEGER AS "numOfTimesOrdered",
+        SUM(oi."quantity")::INTEGER AS "totalUnitsOrdered",
+        p."name",
+        p."description", 
+        p."price",
+        p."stock", 
+        p."categoryName", 
+        p."supplierName", 
+        p."thumbnail"
+      FROM "Product" p
+      JOIN "OrderItem" oi
+      ON p."id" = oi."productId"
+      GROUP BY 1
+      ORDER BY 2 DESC
+      LIMIT ${limit}
+      OFFSET ${(Number(page) - 1) * Number(limit)}
+    `;
+    res.send({bestSellers});
+  } catch (err) {
+    next(err);
+  }
 }
 
 export const getProductById = async (req: Request, res: Response, next: NextFunction) => {
