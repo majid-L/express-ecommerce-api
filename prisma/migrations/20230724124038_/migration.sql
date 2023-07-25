07a00619868b568432c2ff85f84817bb4f579908
3782291211 <Laklouk98@hotmail.com>
2023-07-25 09:33:15 +0100

    Resetting initial migration

---
+++ prisma/migrations/20230724124038_/migration.sql
@@ -0,0 +1,121 @@
+-- CreateTable
+CREATE TABLE "session" (
+    "sid" TEXT NOT NULL,
+    "sess" JSONB NOT NULL,
+    "expire" TIMESTAMP(6) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "customerId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("customerId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(100) NOT NULL,
+    "thumbnail" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "joinDate" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "billingAddress" TEXT,
+    "shippingAddress" TEXT,
+    "avatar" TEXT,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("orderId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "shippingAddress" VARCHAR(100),
+    "status" VARCHAR(20),
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(6,2) NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "categoryName" TEXT NOT NULL,
+    "supplierName" TEXT NOT NULL,
+    "thumbnail" TEXT,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT,
+    "establishYear" INTEGER,
+    "thumbnail" TEXT,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "session_sid_key" ON "session"("sid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_username_key" ON "Customer"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryName_fkey" FOREIGN KEY ("categoryName") REFERENCES "Category"("name") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_supplierName_fkey" FOREIGN KEY ("supplierName") REFERENCES "Supplier"("name") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddCheck
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_min_quantity" CHECK (quantity > 0); 
+
+-- AddCheck
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_min_quantity" CHECK (quantity > 0); 
