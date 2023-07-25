21ced5e341096d5bffcff588a05361d299726ee6
3782291211 <Laklouk98@hotmail.com>
2023-07-25 20:45:25 +0100

    Adding delete account controller

--- controllers/customer.controllers.ts
+++ controllers/customer.controllers.ts
@@ -1,9 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma/prisma';
 
-export const getCustomerById = (req: Request, res: Response, next: NextFunction) => {
+export const getCustomerById = (req: Request, res: Response) => {
+  req.customerDetails.password = '**********';
   res.status(200).send(req.customerDetails);
 }
 
-export const updateAccount = () => {}
-export const deleteAccount = () => {}
+export const deleteAccount = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const deletedUser = await prisma.customer.delete({
+      where: { id: req.customerDetails.id }
+    });
+    res.status(200).send({ 
+      msg: `User ${deletedUser.username} has been deleted.`,
+      deletedUser: {
+        id: deletedUser.id,
+        name: deletedUser.name,
+        username: deletedUser.username,
+        email: deletedUser.email
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+}
