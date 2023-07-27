fb1f270a52ea9d8df1a98e2aef33a772fc6727e9
3782291211 <Laklouk98@hotmail.com>
2023-07-27 12:59:47 +0100

    Refactoring auth validation middleware

--- middleware/validateAuth.ts
+++ middleware/validateAuth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma/prisma';
+import createError from '../helpers/createError';
 
 // Validation middleware for signup
 export const validateUniqueCredentials = async (req: Request, res: Response, next: NextFunction) => {
@@ -13,19 +14,43 @@ export const validateUniqueCredentials = async (req: Request, res: Response, nex
   });
   
   if (customerOrNull) {
-    const err: Error = new Error ('Username/email already in use.');
-    (err as Error & { status: number }).status = 400;
-    next(err);
+    res.status(400).json({ msg: 'Username/email already in use.' });
+  } else {
+    next();
+  }
+}
+
+// Validation middleware for signup
+export const validateAuthInput = (req: Request, res: Response, next: NextFunction) => {
+  const { name = null, username = null, password = null, email = null } = req.body;
+  const requestFieldsAreInvalid = 
+    req.url === '/signup' ?
+      !name || !username || !password || !email
+    : req.url === '/login' ?
+      !username || !password
+    : false;
+  
+  if (requestFieldsAreInvalid) {
+    res.status(400).json({ msg: 'Request body is missing required field(s).' });
   } else {
     next();
   }
 }
 
 // Protect routes from unauthenticated users
-export const userIsAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.passport) {
-    next();
+export const userIsAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session.passport) {
+    res.status(401).json({ msg: 'Unauthenticated.' });
   } else {
-    res.status(401).json({msg: 'Request lacks valid authentication credentials for the requested resource.'});
+    next();
+  }
+}
+
+// Protect PUT/DELETE /api/reviews/:reviewId routes from unauthorised users
+export const userIsAuthorised = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session.passport!.user.id !== req.reviewDetails.customerId) {
+    res.status(403).json({ msg: 'Unauthorised.' });
+  } else {
+    next();
   }
 }
