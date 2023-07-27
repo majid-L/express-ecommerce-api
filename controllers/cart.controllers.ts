bf1a9a464f571bd7cf02c60553fb4b5685ca90f6
3782291211 <Laklouk98@hotmail.com>
2023-07-27 21:53:42 +0100

    Updating cart controller

--- controllers/cart.controllers.ts
+++ controllers/cart.controllers.ts
@@ -1,24 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import prisma from '../prisma/prisma';
+import { emptyCart, insertCartItems, selectCartItems, selectCustomerCartItems } from '../models/cart.models';
 
 export const getCartItems = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const cartItems = req.query.format !== 'basic' ? await prisma.customer.findUnique({
-      where: { id: req.customerDetails.id },
-      select: {
-        id: true,
-        name: true, username: true,
-        cartItems: {
-          select: {
-            quantity: true,
-            product: {}
-          }
-        }
-      }
-    })
-    : await prisma.cartItem.findMany({
-      where: { customerId: req.customerDetails.id }
-    });
+    const cartItems = req.query.format === 'basic' ? 
+      await selectCartItems(req.customerDetails.id)
+      : await selectCustomerCartItems(req.customerDetails.id);
 
     res.status(200).send(cartItems);
   } catch (err) {
@@ -28,19 +15,10 @@ export const getCartItems = async (req: Request, res: Response, next: NextFuncti
 
 export const modifyCart = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    await prisma.cartItem.deleteMany({
-      where: { customerId: req.customerDetails.id }
-    });
-  
+    await emptyCart(req.customerDetails.id);
     if (req.body.length > 0) {
-      await prisma.cartItem.createMany({
-        data: req.body
-      } as { data: CartItem[] });
-
-      const updatedCartItems = await prisma.cartItem.findMany({
-        where: { customerId: req.customerDetails.id }
-      });
-    
+      await insertCartItems(req.body);
+      const updatedCartItems = await selectCartItems(req.customerDetails.id);
       res.status(200).send({updatedCartItems});
     } else {
       res.status(200).send({updatedCartItems: []});
@@ -49,4 +27,3 @@ export const modifyCart = async (req: Request, res: Response, next: NextFunction
     next(err);
   }
 }
-
