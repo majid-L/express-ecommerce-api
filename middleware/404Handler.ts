78ade1e5af2567fa8122145ca41526d552feecc1
3782291211 <Laklouk98@hotmail.com>
2023-07-27 12:59:26 +0100

    Updating 404 handler

--- middleware/404Handler.ts
+++ middleware/404Handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const _404Handler = (req: Request, res: Response) => {
-  res.status(404).send({msg: "No resources are available at this URL."})
+  res.status(404).send({msg: "No routes are mapped to serve this URL."})
 }
 
 export default _404Handler;
