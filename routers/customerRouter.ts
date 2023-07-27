f56f3ed28b52c3e56c08226b11970bf5a29ee654
3782291211 <Laklouk98@hotmail.com>
2023-07-27 13:00:42 +0100

    Adding middleware to customers router

--- routers/customerRouter.ts
+++ routers/customerRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { getCustomerById, deleteAccount } from '../controllers/customer.controllers';
-import validateUpdatedCustomer from '../middleware/validateUpdatedCustomer';
+import { getReviews } from '../controllers/reviews.controller';
+import validateUpdatedModel from '../middleware/validateUpdatedModel';
 import cartRouter from './cartRouter';
 import ordersRouter from './ordersRouter';
 
@@ -11,7 +12,11 @@ customerRouter.use('/orders', ordersRouter);
 customerRouter
 .route('/')
 .get(getCustomerById)
-.put(validateUpdatedCustomer, getCustomerById)
+.put(validateUpdatedModel, getCustomerById)
 .delete(deleteAccount);
+
+customerRouter
+.route('/reviews')
+.get(getReviews);
 
 export default customerRouter;
