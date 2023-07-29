e8ef58d1cc4140e231fc0e9770f63d95036c286f
3782291211 <Laklouk98@hotmail.com>
2023-07-29 23:00:47 +0100

    Refactoring param handler

--- middleware/idParamHandler.ts
+++ middleware/idParamHandler.ts
@@ -3,42 +3,52 @@ import { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma/prisma';
 import createError from '../helpers/createError';
 
+const getModelData = async (url: string, id: number) => {
+  let objectOrNull: ModelObject | null = null;
+  const options = { where: { id } };
+
+  if (url.includes('customers')) {
+    objectOrNull = await prisma.customer.findUnique({
+      ...options, 
+      include: { 
+        billingAddress: {},
+        shippingAddress: {}
+      }
+    });
+  } else if (url.includes('products')) {
+    objectOrNull = await prisma.product.findUnique(options);
+  } else if (url.includes('reviews')) {
+    objectOrNull = await prisma.review.findUnique(options);
+  }
+  return objectOrNull
+}
+
+const attachModelDataToRequest = (req: Request, modelObject: ModelObject) => {
+  if (req.originalUrl.includes('customers')) {
+    req.customerDetails = modelObject as Prisma.CustomerGetPayload<{}>;
+  } else if (req.originalUrl.includes('products')) {
+    req.productDetails = modelObject as Prisma.ProductGetPayload<{}>;
+  } else if (req.originalUrl.includes('reviews')) {
+    req.reviewDetails = modelObject as Prisma.ReviewGetPayload<{}>;
+  }
+}
+
 const idParamHandler = async (req: Request, res: Response, next: NextFunction, id: string) => {
   try {
-    const paramIsCustomerId = req.originalUrl.includes('customers');
-    const paramIsProductId = req.originalUrl.includes('products');
-    const paramIsReviewId = req.originalUrl.includes('reviews');
-
     if (isNaN(Number(id))) {
       return next(createError('Id must be a number.', 400));
     }
 
-    let objectOrNull: ModelObject | null = null;
-    const options = { where: { id: Number(id) } };
-
-    if (paramIsCustomerId) {
-      objectOrNull = await prisma.customer.findUnique(options);
-    } else if (paramIsProductId) {
-      objectOrNull = await prisma.product.findUnique(options);
-    } else if (paramIsReviewId) {
-      objectOrNull = await prisma.review.findUnique(options);
-    }
+    const objectOrNull = await getModelData(req.originalUrl, Number(id));
 
     if (!objectOrNull) return next(createError('Not found.', 404));
-    if (paramIsCustomerId 
+    if (req.originalUrl.includes('customers') 
       && !/\/\d+\/reviews.*$/i.test(req.originalUrl)
       && req.session.passport!.user.id !== Number(id)) {
       return next(createError('Unauthorised.', 403));
     }
-
-    if (paramIsCustomerId) {
-      req.customerDetails = objectOrNull as Prisma.CustomerGetPayload<{}>;
-    } else if (paramIsProductId) {
-      req.productDetails = objectOrNull as Prisma.ProductGetPayload<{}>;
-    } else if (paramIsReviewId) {
-      req.reviewDetails = objectOrNull as Prisma.ReviewGetPayload<{}>;
-    }
-
+    
+    attachModelDataToRequest(req, objectOrNull);
     next();
   } catch (err) {
     next(err);
