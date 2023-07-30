a20b2b1dc8ae8a4303d2f7a1a31744f02ab963db
3782291211 <Laklouk98@hotmail.com>
2023-07-30 21:06:00 +0100

    Updating auth tests

--- api_tests/auth.spec.ts
+++ api_tests/auth.spec.ts
@@ -3,6 +3,7 @@ import app from '../index';
 import { expect } from "chai";
 import bcrypt from 'bcrypt';
 import { cookie } from "./index.spec";
+import { setupFunction } from "./index.spec";
 
 const authTests = () => {
   describe('Authorisation and authentication tests', () => {
@@ -51,6 +52,8 @@ const authTests = () => {
     });
 
     describe('/api/signup', () => {
+      beforeEach(setupFunction);
+
       it('Customer can sign up for a new account.', async () => {
         const { body }: { body: Customer } = await request(app)
           .post('/api/signup')
@@ -62,7 +65,7 @@ const authTests = () => {
           })
           .expect(201);
       
-        expect(body).to.be.an('object').that.has.all.keys('id', 'name', 'username', 'password', 'email', 'joinDate', 'billingAddress', 'shippingAddress', 'avatar');
+        expect(body).to.be.an('object').that.has.all.keys('id', 'name', 'username', 'password', 'email', 'joinDate', 'billingAddressId', 'shippingAddressId', 'phone', 'avatar');
         expect(body.name).to.equal('Kal Varrick');
         expect(body.username).to.equal('kvarrick3000');
         expect(body.email).to.equal('kvarrick@taliphus.ga');
@@ -72,7 +75,7 @@ const authTests = () => {
       });
   
       it('Rejects attempts to sign up using an existing username or email.', async () => {
-        const signupWithExistingUsername = await request(app)
+        const { body: firstErrorResponse }: ApiErrorResponse = await request(app)
           .post('/api/signup')
           .send({
             name: "Marcus Boone",
@@ -82,9 +85,9 @@ const authTests = () => {
           })
           .expect(400);
   
-        expect(signupWithExistingUsername.body.msg).to.equal('Username/email already in use.');
+        expect(firstErrorResponse.error.info).to.equal('Username/email already in use.');
   
-        const signupWithExistingEmail = await request(app)
+        const { body: secondErrorResponse }: ApiErrorResponse = await request(app)
           .post('/api/signup')
           .send({
             name: "Marcus Boone",
@@ -94,7 +97,7 @@ const authTests = () => {
           })
           .expect(400);
   
-        expect(signupWithExistingEmail.body.msg).to.equal('Username/email already in use.');
+        expect(secondErrorResponse.error.info).to.equal('Username/email already in use.');
       });
     });
 
@@ -116,158 +119,174 @@ const authTests = () => {
       });
     
       it('Rejects unauthenticated access.', async () => {
-        const logoutResponse = await request(app)
+        const { body: logoutResponse }: ApiErrorResponse = await request(app)
           .post('/api/logout')
           .expect(401);
         
-        expect(logoutResponse.body.msg).to.equal('Unauthenticated.');
+        expect(logoutResponse.error.info).to.equal('Unauthenticated.');
       })
     });
 
     describe('Authenticated routes.', () => {
       it('GET /api/customers/:customerId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/1')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('PUT /api/customers/:customerId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .put('/api/customers/1')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('DELETE /api/customers/:customerId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .delete('/api/customers/1')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('GET /api/customers/:customerId/orders forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/1/orders')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('POST /api/customers/:customerId/orders forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .post('/api/customers/1/orders')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('GET /api/customers/:customerId/orders/:orderId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/1/orders/21')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('GET /api/customers/:customerId/cart forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/1/cart')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('POST /api/reviews/ forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .post('/api/reviews')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('PUT /api/reviews/:reviewId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .put('/api/reviews/1')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
 
       it('DELETE /api/reviews/:reviewId forbids unauthenticated access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .delete('/api/reviews/1')
           .expect(401);
         
-        expect(msg).to.equal('Unauthenticated.');
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
+      });
+
+      it('POST /api/customers/:id/addresses forbids unauthenticated access.', async () => {
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
+          .post('/api/customers/1/addresses')
+          .expect(401);
+        
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
+      });
+
+      it('DELETE /api/customers/:id/addresses/:id forbids unauthenticated access.', async () => {
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
+          .delete('/api/customers/1/addresses/8')
+          .expect(401);
+        
+        expect(errorResponse.error.info).to.equal('Unauthenticated.');
       });
     });
 
     describe('Authorised routes.', () => {
       it('GET /api/customers/:customerId forbids unauthorised access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/3')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('PUT /api/customers/:customerId forbids unauthorised access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .put('/api/customers/3')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('DELETE /api/customers/:customerId forbids unauthorised access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .delete('/api/customers/3')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('GET /api/customers/:customerId/orders forbids unauthorised access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/3/orders')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('GET /api/customers/:customerId/cart forbids unauthorised access.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
           .get('/api/customers/3/cart')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('PUT /api/reviews/:reviewId forbids user from modifying another customer\'s review.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
-          .put('/api/reviews/55')
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
+          .put('/api/reviews/14')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
       it('DELETE /api/reviews/:reviewId forbids user from deleting another customer\'s review.', async () => {
-        const { body: { msg} }: ApiErrorResponse = await request(app)
-          .delete('/api/reviews/54')
+        const { body: errorResponse }: ApiErrorResponse = await request(app)
+          .delete('/api/reviews/7')
           .set('Cookie', cookie)
           .expect(403);
         
-        expect(msg).to.equal('Unauthorised.');
+        expect(errorResponse.error.info).to.equal('Unauthorised.');
       });
 
     });
