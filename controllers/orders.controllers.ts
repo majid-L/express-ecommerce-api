6053503e46e4100bf6b436c2d039fa6ebac7852d
3782291211 <Laklouk98@hotmail.com>
2023-07-31 06:29:37 +0100

    Updating controllers

--- controllers/orders.controllers.ts
+++ controllers/orders.controllers.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import generateArray from '../helpers/generateArray';
 import { insertOrder, selectOrderById, selectOrders, updateStockAmounts } from '../models/orders.models';
-import { emptyCart, selectCartItems } from '../models/cart.models';
+import { 
+  emptyCartOrWishlist as emptyCart, 
+  selectCartOrWishlistItems as selectCartItems 
+} from '../models/cart.models';
 import prisma from '../prisma/prisma';
+import { Prisma } from '@prisma/client';
+import createError from '../helpers/createError';
 
 export const getOrders = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -19,7 +24,7 @@ export const getOrderById = async (req: Request, res: Response, next: NextFuncti
     if (orderOrNull) {
       res.status(200).send(orderOrNull);
     } else {
-      res.status(404).send('Not found.');
+      return next(createError('Not found.', 404));
     }
   } catch (err) {
     next(err);
@@ -28,28 +33,36 @@ export const getOrderById = async (req: Request, res: Response, next: NextFuncti
 
 export const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // Get all the items in the customer's cart
-    const cartItems = await selectCartItems(req.customerDetails.id);
-    if (cartItems.length === 0) {
-      return res.status(400).send({ msg: "Unable to place order because cart is empty."});
+    // Create a new order that will be referenced by the order items below
+    const newOrder = await insertOrder(
+      req.customerDetails,
+      (req.addresses.billingAddress as Prisma.AddressGetPayload<{}>).id,
+      (req.addresses.shippingAddress as Prisma.AddressGetPayload<{}>).id
+    );
+
+    let orderItems = [] as OrderItem[];
+    if (!req.body.item) {
+      // Get all the items in the customer's cart
+      const cartItems = await selectCartItems(req.customerDetails.id, 'basic', 'cartItems');
+      if ((cartItems as CartItem[]).length === 0) {
+        return next(createError('Unable to place order because cart is empty.', 400));
+      }
+      // Generate an array of order items
+      orderItems = generateArray(cartItems as CartItem[], newOrder.id); 
+    } else {
+      orderItems = generateArray([req.body.item], newOrder.id);
     }
 
-    // Create a new order that will be referenced by the order items below
-    const newOrder = await insertOrder(req.customerDetails);
-
-    // Generate an array of order items
-    const orderItems: OrderItem[] = generateArray(cartItems, newOrder.id); 
-
     // Now add all those order items to the order items table, completing the transaction
-    await prisma.orderItem.createMany({ data: orderItems });
+    if (orderItems.length === 1) {
+      await prisma.orderItem.create({ data: orderItems[0] });
+    } else {
+      await prisma.orderItem.createMany({ data: orderItems });
+    }
 
-    // Subtract order quantities from product stock
+    // Subtract order quantities from product stock, clear the cart and return the new order
     await updateStockAmounts(orderItems);
-
-    // Clear the cart
     await emptyCart(req.customerDetails.id );
-
-    // Finally, return the new order
     const completedOrder = await selectOrderById(newOrder.id);
     res.status(201).send(completedOrder);
   } catch (err) {
--- controllers/products.controllers.ts
+++ controllers/products.controllers.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import prisma from '../prisma/prisma';
 import { selectBestsellers, selectProductById, selectProducts } from '../models/products.models';
 
 export const getProducts = async (
