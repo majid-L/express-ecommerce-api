76410694ef0501236239fe698e14a337074874dd
3782291211 <Laklouk98@hotmail.com>
2023-08-01 09:09:30 +0100

    Enabling trust proxy for production server

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import _404Handler from './middleware/404Handler';
 import validateRequestMethod from './middleware/validateRequestMethod';
 
 const app: express.Application = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 
