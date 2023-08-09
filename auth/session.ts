220c7de2e53231760d62f5ee14ad4dcd576c9715
3782291211 <Laklouk98@hotmail.com>
2023-08-09 19:07:36 +0100

    Restoring cookie settings

--- auth/session.ts
+++ auth/session.ts
@@ -38,8 +38,8 @@ const generateSession = (app: Application) => {
       secret: process.env.SESSION_SECRET as string,
       cookie: { 
         maxAge: 30 * 24 * 60 * 60 * 1000, 
-        secure: false,
-        httpOnly: false,
+        secure,
+        httpOnly: secure,
         sameSite: 'none' 
       },
       saveUninitialized: false,
