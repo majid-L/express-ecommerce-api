bac018e69e6817aea5f5564f7db96809b81cc1bc
majid-L <Laklouk98@hotmail.com>
2024-02-07 00:35:09 +0000

    Adding origin

--- index.ts
+++ index.ts
@@ -13,8 +13,7 @@ const app: express.Application = express();
 app.set('trust proxy', 1);
 app.use(flash());
 app.use(cors({
-    origin: ['http://localhost:4200', 'https://localhost:4200', 'http://127.0.0.1:5501', 'https://xpulse.vercel.app'],
-    //origin: "*",
+    origin: ['http://localhost:4200', 'https://localhost:4200', 'http://127.0.0.1:5501', 'https://xpulse.vercel.app', 'https://majid-l.github.io'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
