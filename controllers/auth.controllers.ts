80f2a3c44943331e03fda2eb28151b9486ac945c
majid-L <Laklouk98@hotmail.com>
2024-02-12 06:50:11 +0000

    Adding cookie parser middleware

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -1,78 +1,114 @@
-import { Request, Response, NextFunction } from 'express';
-import prisma from '../prisma/prisma';
-import bcrypt from 'bcrypt';
-import hidePassword from '../helpers/hidePassword';
-import createError from '../helpers/createError';
-import { Prisma } from '@prisma/client';
+import { Request, Response, NextFunction } from "express";
+import prisma from "../prisma/prisma";
+import bcrypt from "bcrypt";
+import hidePassword from "../helpers/hidePassword";
+import createError from "../helpers/createError";
+import { Prisma } from "@prisma/client";
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const data = (({ username, name, email }) => 
-      ({ username, name, email, password: hashedPassword }))(req.body);
+    const data = (({ username, name, email }) => ({
+      username,
+      name,
+      email,
+      password: hashedPassword,
+    }))(req.body);
     const newCustomer = await prisma.customer.create({ data });
-    req.login(newCustomer, err => {
+    req.login(newCustomer, (err) => {
       if (err) return next(err);
+
+      // set cookie
+      res.cookie("xpulse.userid", newCustomer.id, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      });
+
       res.status(201).send({
-        customer: hidePassword(newCustomer as User)
+        customer: hidePassword(newCustomer as User),
       });
     });
   } catch (err) {
     next(err);
   }
-}
+};
 
-export const authenticateWithSSO = async (req: Request<{}, {}, SSORequestFields>, res: Response, next: NextFunction) => {
+export const authenticateWithSSO = async (
+  req: Request<{}, {}, SSORequestFields>,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { name, email, authId, thumbnail, provider } = req.body;
 
     for (const field of ["name", "email", "authId", "provider"]) {
       if (!req.body[field as keyof SSORequestFields]) {
-        return next(createError('Request body is missing required field(s).', 400));
+        return next(
+          createError("Request body is missing required field(s).", 400)
+        );
       }
     }
 
     const data = { name, username: email, email, avatar: thumbnail };
     let customer: Prisma.CustomerGetPayload<{}>;
-    const oAuthProfileOrNull = await prisma.oAuthProfile.findFirst({ where: { authId, provider } });
- 
+    const oAuthProfileOrNull = await prisma.oAuthProfile.findFirst({
+      where: { authId, provider },
+    });
+
     if (oAuthProfileOrNull) {
       customer = await prisma.customer.upsert({
         where: { id: oAuthProfileOrNull.customerId },
         update: data,
-        create: data
+        create: data,
       });
     } else {
       customer = await prisma.customer.create({ data });
       await prisma.oAuthProfile.create({
-        data: { authId, customerId: customer.id, provider } 
+        data: { authId, customerId: customer.id, provider },
       });
     }
 
-    req.login(customer, err => {
+    // set cookie
+    res.cookie("xpulse.userid", customer.id, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    req.login(customer, (err) => {
       if (err) return next(err);
       res.status(201).send({ customer });
     });
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
-}
+};
 
 export const login = (req: Request, res: Response) => {
-  res.status(200).send({
-    customer: hidePassword(req.user as User)
+  // set cookie
+  res.cookie("xpulse.userid", (req.user as User).id, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
-}
+
+  res.status(200).send({
+    customer: hidePassword(req.user as User),
+  });
+};
 
 export const logout = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization === "swagger ui") {
-    return res.send({ msg: 'Successfully logged out.' });
+    return res.send({ msg: "Successfully logged out." });
   }
 
   const username = (req.user as User).username;
+
+  // Delete cookie
+  res.clearCookie("xpulse.userid");
+
   req.logout((err: Error) => {
     if (err) return next(err);
-    res.send({ msg: username + ' is now logged out.' });
+    res.send({ msg: username + " is now logged out." });
   });
-}
+};
--- index.ts
+++ index.ts
@@ -1,31 +1,41 @@
-import express from 'express';
-import { userIsAuthenticated } from './middleware/validateAuth';
-import cors from 'cors';
-import handleError from './middleware/errors';
-import apiRouter from './routers/apiRouter';
-import passportConfig from './auth/passport';
-import generateSession from './auth/session';
-import _404Handler from './middleware/404Handler';
-import validateRequestMethod from './middleware/validateRequestMethod';
-import flash from 'express-flash';
+import express from "express";
+import { userIsAuthenticated } from "./middleware/validateAuth";
+import cors from "cors";
+import handleError from "./middleware/errors";
+import apiRouter from "./routers/apiRouter";
+import passportConfig from "./auth/passport";
+import generateSession from "./auth/session";
+import _404Handler from "./middleware/404Handler";
+import validateRequestMethod from "./middleware/validateRequestMethod";
+import flash from "express-flash";
+import cookieParser from "cookie-parser";
 
 const app: express.Application = express();
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
+app.use(cookieParser());
 app.use(flash());
-app.use(cors({
-    origin: ['http://localhost:4200', 'https://localhost:4200', 'http://127.0.0.1:5501', 'https://xpulse.vercel.app', 'https://majid-l.github.io'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4200",
+      "https://localhost:4200",
+      "http://127.0.0.1:5501",
+      "https://xpulse.vercel.app",
+      "https://majid-l.github.io",
+    ],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 generateSession(app);
 passportConfig(app);
 
 app.use(validateRequestMethod);
-app.use('/api/customers/:customerId', userIsAuthenticated);
+app.use("/api/customers/:customerId", userIsAuthenticated);
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.use(handleError);
 app.use(_404Handler);
